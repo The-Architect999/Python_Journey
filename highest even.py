511d8f29cfbe924eafae7939d301416165932c78
The-Architect999 <joysaldanha1@gmail.com>
2026-01-24 18:47:43 +0530

    highest even

---
+++ highest even.py
@@ -0,0 +1,11 @@
+def highest_even(a):
+    even = []
+    highest = 0
+    for numbers in a:
+        if numbers % 2 == 0:
+            even.append(numbers)
+    for nos in even:
+        if (nos - highest) > 0:
+            highest = (nos)
+    return(highest)
+print(highest_even([10,2,3,4,8,11]))
