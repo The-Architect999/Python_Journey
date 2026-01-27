055b52dc3a282f85ce20d5c964112bafed072ab4
The-Architect999 <joysaldanha1@gmail.com>
2026-01-27 14:54:11 +0530

    list_normalizer

--- list_normalizer.py
+++ list_normalizer.py
@@ -1,9 +1,13 @@
 messy_names = ["  albert ", " bOB  ", "   cArlOs", "ALICE   ", " bOb "]
 temp_list = []
+role_dict = {}
 for names in messy_names:
     names = names.strip().capitalize()
     if names not in temp_list:
         temp_list.append(names)
-print(temp_list)
+for name in temp_list:
+    initials = name[0]
+    role_dict[initials] = name
+print(role_dict)
 
     
