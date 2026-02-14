0a07126c42c9a9987c672b404f92c8fb096a5da2
The-Architect999 <joysaldanha1@gmail.com>
2026-02-14 15:56:08 +0530

    updated_cans

--- clash_of_cans.py
+++ clash_of_cans.py
@@ -1,78 +1,82 @@
 # clash of cans
 
+# need to add a few dictionaries that store all the information on levels and stats
+
 # recalls everything at one place
 class IngameObjects():
     pass
 
-# to use as a call back for every army function and building
 
-
-class Army(IngameObjects):
-    def __init__(troop_capacity):
-        pass
-
-# parent class - Troops
-
-
-class Troops(Army):
-    def __init__(self, types, dammage, health):
-        self.types = types
-        self.dammage = dammage
-        self.health = health
-
-    def attack(self, target):
-        target -= self.dammage
-        return f"{self.name} attacked {target} for {self.damage} damage!"
-
-
-# children -actual troops to assign attributes and set defaults
-class Archers (Troops):
-    def __init__(self, location, quant):
-        self.location = location
-        self.quant = quant
+class Troops(IngameObjects):
+    def __init__(self):
+        # default counters, to be updated with the library
+        self.dammage = 0
+        self.health = 0
+        self.quant = 0
         self.level = 1
 
-    def deploy(self, location):
-        self.location = tuple(location)
-        # to code after learning inputs - GUI
+    def deploy(self, location: tuple):
+        self.loaction = location
+        # to learn more to add here
+        # need to check if i should use a __srt__ or just add a name in args
+        return f"{self} Deployed!"
+
+    def Train(self, quantity):
+        self.quantity = quantity
+
+
+# children - Troolps to set defaults
+class Archers (Troops):
+    def __init__(self):
         pass
+
+
+# only one instance needed
+archer = Archers()
 
 
 class Barbarians (Troops):
-    def __init__(self, location, quant):
-        self.location = location
-        self.quant = quant
-
-    def deploy(self, location):
-        self.location = tuple(location)
-        # to code after learning inputs - GUI
+    def __init__(self):
         pass
+
+
+# only one instance needed
+barbarian = Barbarians()
 
 
 class WallBreakers (Troops):
-    def __init__(self, location, quant):
-        self.location = location
-        self.quant = quant
-
-    def deploy(self, location):
-        self.location = tuple(location)
-        # to code after learning inputs - GUI
+    def __init__(self):
         pass
+
+
+# only one instance needed
+wallbreaker = WallBreakers()
+
 
 # class of defences:
+class Defences(IngameObjects):
+    def __init__(self, location):
+        self.loaction = location
+        self.status = "idle"
 
-
-class Defences:
-    def active(self, status):
-        self.status = status
-        if status == "under construction":
-            return f"{self} is currently being upgraded!"
-
-    def upgrade(self, level):
-        # i think it will be better if i store the data outside the classes
-        # as when i call them, i can use: class(input, stored data)
+    def active(self):
+        self.level = 1
+        self.status = False
+        # not sure if i should set the status to a bool or a str "currently upgrading"
         # to explore
-        pass
+
+    def upgrade(self):
+        if self.status == "upgrading":
+            return f"Building already being upgraded!"
+        else:
+            # to code for cancel upgrade
+            # to do the cost and time codint - yet to learn
+            self.level += 1
+            # time and level check needed
+            # also want to add a want to cancel upgrade code
+            return f"{self} is now being upgraded to {self.level}"
+            # i think ill need to add names in the args as well to get it to print the building name
+            # need to check for alternatives to do that as i already have the name as class when i need to call it
 
     def defend(self, under_attack):
         self.under_attack = under_attack
@@ -82,54 +86,35 @@ class Defences:
             # need to get range here
             pass
 
+# yet to check what args to pass to parent for defences
+
 
 class ArcherTower(Defences):
-    def __init__(self, location, types, damage, health):
-        # will set location as a tuple for the top left tile to the bottom right tile
-        self.location = location
-        self.types = types
-        self.damage = damage
-        self.health = health
-
-    def defend(self):
-        # to code
-        pass
+    def __init__(self, location: tuple):
+        super().__init__(location)
 
 
 class Cannon(Defences):
-    def __init__(self, location, types, damage, health):
-        self.location = location
-        self.types = types
-        self.damage = damage
-        self.health = health
-
-    def defend(self):
-        # to code
-        pass
+    def __init__(self, location: tuple):
+        super().__init__(location)
 
 
 class Mortar(Defences):
-    def __init__(self, location, types, damage, health):
-        self.location = location
-        self.types = types
-        self.damage = damage
-        self.health = health
+    def __init__(self, location: tuple):
+        super().__init__(location)
 
-    def defend(self):
+
+# Class of empire buildings:
+class MainBuildings(IngameObjects):
+    def __init__(self):
+        self.level = 1
         # to code
         pass
 
 
-# Class of empire buildings:
-class MainBuildings:
-    # to code
-    pass
-
-
 class Townhall(MainBuildings):
-    def __init__(self, damage, health):
-        self.damage = damage
-        self.health = health
+    def __init__(self):
+        pass
 
     def nothing(self):
         # to code or not to code that is the question
@@ -137,12 +122,11 @@ class Townhall(MainBuildings):
 
 
 class ClanCastle(MainBuildings):
-    def __init__(self, damage, health):
-        self.damage = damage
-        self.health = health
+    def __init__(self, level):
+        self.level = 1
 
     def requesttroops(self):
-        # to
+        # to code
         pass
 
 
